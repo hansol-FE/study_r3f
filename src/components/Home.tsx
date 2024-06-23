9c05c0ad893a88c628ec54068e4ea56735d30602
hansol <dundunki@naver.com>
2024-06-23 16:36:54 +0900

    drei의 CameraControls 사용

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import ShowRoom from "@components/three/ShowRoom";
-import { OrbitControls } from "@react-three/drei";
 
 const Home = () => {
   return (
     <>
-      <Canvas>
+      <Canvas shadows>
         <axesHelper args={[5]} />
         <gridHelper />
 
-        {/* 마우스로 컨트롤 가능  */}
-        <OrbitControls />
-
-        {/* (3,3,3) position에서 빛을 줌 */}
-        <directionalLight position={[3, 3, 3]} />
+        {/* 카메라 컨트롤 가능  */}
+        {/* <OrbitControls /> */}
 
         <ShowRoom />
       </Canvas>
--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,14 +1,18 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useThree } from "@react-three/fiber";
+import { CameraControls } from "@react-three/drei";
+import { useEffect, useRef, useState } from "react";
 
 const ShowRoom = () => {
   const gltf = useLoader(GLTFLoader, "./models/custom.glb");
 
   const { raycaster } = useThree();
+  const cameraControlsRef = useRef<CameraControls>(null);
+  const [isFocus, setIsFocus] = useState(false);
 
-  const shoesClick = () => {
+  const shoesClick = (e: any) => {
     const intersectObjs = raycaster.intersectObjects(gltf.scene.children, true);
     console.log("intersectObjs", intersectObjs);
 
@@ -22,11 +26,75 @@ const ShowRoom = () => {
 
       const cloneMaterial = firstObj.material as THREE.MeshStandardMaterial;
       cloneMaterial.color = new THREE.Color("red");
+
+      // cameraControlsRef.current?.setLookAt(
+      //   -2,
+      //   0,
+      //   2,
+      //   firstObj.position.x,
+      //   firstObj.position.y,
+      //   firstObj.position.z,
+      //   true
+      // );
+
+      // setIsFocus(true);
+      cameraControlsRef.current?.fitToBox(firstObj, true).then(() => {
+        setIsFocus(false);
+      });
     }
   };
 
+  let angle = 0;
+  let distance = 1.2;
+  useFrame(() => {
+    // 턴테이블 효과
+    // if (!isFocus) {
+    //   cameraControlsRef.current?.setPosition(
+    //     distance * Math.sin(angle),
+    //     0.8,
+    //     distance * Math.cos(angle),
+    //     true
+    //   );
+    //   angle = angle + 0.01;
+    // }
+
+    const rightShoes = gltf.scene.children[0];
+    const leftShoes = gltf.scene.children[1];
+
+    leftShoes.rotation.y = THREE.MathUtils.degToRad(300);
+    // leftShoes.rotation.z = 0.3;
+  });
+
+  useEffect(() => {
+    cameraControlsRef.current?.setTarget(0, 0, 0, false);
+    cameraControlsRef.current?.addEventListener("control", () => {
+      setIsFocus(true);
+    });
+
+    cameraControlsRef.current?.addEventListener("sleep", () => {
+      setIsFocus(false);
+    });
+  }, []);
+
   return (
     <>
+      {/* (3,3,3) position에서 빛을 줌 */}
+      <directionalLight position={[3, 3, 3]} castShadow />
+      <pointLight position={[0, 1, 0]} intensity={3} castShadow />
+
+      <CameraControls
+        ref={cameraControlsRef}
+        enabled={true}
+        dollyToCursor={true}
+        minDistance={0.5}
+        // maxDistance={10}
+      />
+
+      <mesh scale={4} position={[0, -0.4, 0]} castShadow receiveShadow>
+        <cylinderGeometry args={[0.4, 0.2, 0.2, 30]} />
+        <meshStandardMaterial />
+      </mesh>
+
       <primitive object={gltf.scene} onClick={shoesClick} />
       {/* <mesh
         rotation={[

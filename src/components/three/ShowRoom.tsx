00dd954c8faebd59de2d7d10607c32067350594b
hansol <dundunki@naver.com>
2024-06-23 15:21:30 +0900

    모델에 클릭 이벤트 달고 색상 바꾸기

--- src/components/three/ShowRoom.tsx
+++ src/components/three/ShowRoom.tsx
@@ -1,13 +1,33 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { useLoader } from "@react-three/fiber";
+import { useThree } from "@react-three/fiber";
 
 const ShowRoom = () => {
   const gltf = useLoader(GLTFLoader, "./models/custom.glb");
 
+  const { raycaster } = useThree();
+
+  const shoesClick = () => {
+    const intersectObjs = raycaster.intersectObjects(gltf.scene.children, true);
+    console.log("intersectObjs", intersectObjs);
+
+    if (intersectObjs.length > 0) {
+      const firstObj = intersectObjs[0].object as THREE.Mesh;
+      console.log("firstObj.name?", firstObj.name);
+      const firstMaterial = firstObj.material as THREE.MeshStandardMaterial;
+
+      const clone = firstMaterial.clone();
+      firstObj.material = clone;
+
+      const cloneMaterial = firstObj.material as THREE.MeshStandardMaterial;
+      cloneMaterial.color = new THREE.Color("red");
+    }
+  };
+
   return (
     <>
-      <primitive object={gltf.scene} />
+      <primitive object={gltf.scene} onClick={shoesClick} />
       {/* <mesh
         rotation={[
           THREE.MathUtils.degToRad(45), //x축
